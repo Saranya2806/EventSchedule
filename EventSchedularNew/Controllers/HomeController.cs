aca37ad702ea3485bb7d2984152b62d06bc3129c
saranya.p <saranya.p@STPLPC666>
2022-12-22 13:24:48 +0530

    Add project files.

---
+++ EventSchedularNew/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventSchedularNew.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public JsonResult GetEvents()
+        {
+            using (ConferenceHallEntities3 obj = new ConferenceHallEntities3())
+            {
+                var events = obj.NewEventsDatas.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+            }        
+        }
+    }
+}
